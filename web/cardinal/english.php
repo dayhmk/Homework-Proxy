501e07511892d4d6531dee1b84ab3fa746c36d8f
dayhmk <dayhomework@gmail.com>
2016-01-13 16:49:30 -0500

    Remove echo

--- web/cardinal/english.php
+++ web/cardinal/english.php
@@ -2,7 +2,6 @@
 	header('Access-Control-Allow-Origin: http://dayhmk.github.io');
 	require '../utils.php';
 	$text = util_rss("https://zappdragons.wordpress.com/feed", "homework");
-	echo $text;
 	$text = util_split("/(english:|english)/i", $text, 1, 1);
 	$text = util_split("/(english|math|science|history)/i", $text, 0, 0);
 	$text = util_split('/<a rel="nofollow"/i', $text, 0, 0);
