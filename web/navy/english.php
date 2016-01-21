aac80cb236995e7c7c23054256de1e4c61aa593b
dayhmk <dayhomework@gmail.com>
2016-01-21 18:03:27 -0500

    Added Navy Homework

---
+++ web/navy/english.php
@@ -0,0 +1,10 @@
+<?php
+	header('Access-Control-Allow-Origin: http://dayhmk.github.io');
+	require '../utils.php';
+	$text = file_get_contents("http://www2.newton.k12.ma.us/~kathy_maher/?OpenItemURL=S054D0D5C");
+	$text = util_split("/for Navy English and Social Studies/i", $text, 1, 1);
+	$text = util_split('/<table[^>]*>/i', $text, 1, 0);
+	$text = preg_split('/<tr[^>]*>/i', $text)[3];
+	$text = preg_split('/<td[^>]*>/i', $text)[1];
+	echo strip_tags($text, '<br>');
+?>
---
+++ web/navy/math.php
@@ -0,0 +1,10 @@
+<?php
+	header('Access-Control-Allow-Origin: http://dayhmk.github.io');
+	require '../utils.php';
+	$text = file_get_contents("http://www2.newton.k12.ma.us/~benjamin_gresser/?OpenItemURL=S0B6220A6");
+	$text = util_split("/<b>new<\/b>/i", $text, 1, 1);
+	$text = util_split('/<table[^>]*>/i', $text, 1, 0);
+	$text = preg_split('/<tr[^>]*>/i', $text)[1];
+	$text = preg_split('/<td[^>]*>/i', $text)[4];
+	echo strip_tags($text, '<br>');
+?>
