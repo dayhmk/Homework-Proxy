0240a74be75e3caaf430507bb6f98cf7409990df
dayhmk <dayhomework@gmail.com>
2016-02-20 10:24:21 -0500

    Fixed bug with 404

--- web/404.php
+++ web/404.php
@@ -1,5 +1,5 @@
 <?php
-	header('Access-Control-Allow-Origin: http://dayhmk.github.io');
+	header('Access-Control-Allow-Origin: https://dayhmk.github.io');
 	header("Status: 404 Not Found");
 	echo "<script>window.location = 'http://dayhmk.github.io';</script>";
 ?>
