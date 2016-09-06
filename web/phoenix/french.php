cc38913e0097e5c997eeb3f6275548bc9148ff13
DayHMK <dayhomework@gmail.com>
2016-09-06 17:15:37 -0400

    Fixed Phoenix and Evergreen French scripts

--- web/phoenix/french.php
+++ web/phoenix/french.php
@@ -1,8 +1,11 @@
 <?php
 	header('Access-Control-Allow-Origin: https://dayhmk.github.io');
 	require '../utils.php';
+
 	$text = file_get_contents("http://www2.newton.k12.ma.us/~catherine_hibbard/?OpenItemURL=S0623DE6D");
-	$text = util_split('/(janvier|février|mars|avril|mai|juin|juillet|août|septembre|octobre|novembre|décembre)/i', $text, -1, 0);
-	$text = util_split('/<div[^>]*>/i', $text, -3, -1);
+	$text = util_split('/(janvier|février|mars|avril|mai|juin|juillet|août|septembre|octobre|novembre|décembre)/i', $text, -1, SPLIT_ONE);
+	$text = util_split('/<div[^>]*>/i', $text, -3, SPLIT_AND_LOWER);
+	$text = util_split('/(lundi|mardi|mercredi|jeudi|vendredi)/i', $text, -2, SPLIT_AND_LOWER);
+
 	echo_json(finalize($text), "http://www2.newton.k12.ma.us/~catherine_hibbard/?OpenItemURL=S0623DE6D");
 ?>
