d979730e64e99f838d4323aec2fbca98894b335c
DayHMK <dayhomework@gmail.com>
2016-09-06 16:52:00 -0400

    Fixed a broken link

--- web/navy/history.php
+++ web/navy/history.php
@@ -6,5 +6,5 @@
 	$text = util_split('/<table[^>]*>/i', $text, 1, 0);
 	$text = preg_split('/<tr[^>]*>/i', $text)[3];
 	$text = preg_split('/<td[^>]*>/i', $text)[2];
-	echo_json(finalize($text), "http://www2.newton.k12.ma.us/~courtney_fournier/?OpenItemURL=S08BD3AB2");
+	echo_json(finalize($text), "http://www2.newton.k12.ma.us/~kathy_maher/?OpenItemURL=S054D0D5C");
 ?>
