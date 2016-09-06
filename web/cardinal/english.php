176323f70fe598447aca985e501d6021c9a5b7bc
DayHMK <dayhomework@gmail.com>
2016-09-06 16:11:43 -0400

    Changed source for Cardinal homework

--- web/cardinal/english.php
+++ web/cardinal/english.php
@@ -1,9 +1,9 @@
 <?php
 	header('Access-Control-Allow-Origin: https://dayhmk.github.io');
 	require '../utils.php';
-	$text = util_rss("https://zappdragons.wordpress.com/feed", "homework");
+	$text = util_blogspot("http://cardinalnationnews.blogspot.com/feeds/posts/default");
 	$text = util_split("/(english:|english)/i", $text, 1, 1);
 	$text = util_split("/(english|math|science|history)/i", $text, 0, 0);
 	$text = util_split('/<a rel="nofollow"/i', $text, 0, 0);
-	echo_json(finalize($text), "https://zappdragons.wordpress.com/", "zappdragons");
+	echo_json(finalize($text), "https://cardinalnationnews.blogspot.com/", "Cardinal Nation News");
 ?>
--- web/fire/history.php
+++ web/fire/history.php
@@ -1,7 +1,7 @@
 <?php
 	header('Access-Control-Allow-Origin: https://dayhmk.github.io');
 	require '../utils.php';
-	$text = util_blogspot("http://fireteamhistory.blogspot.com/feeds/posts/default/", "homework");
+	$text = util_blogspot("http://fireteamhistory.blogspot.com/feeds/posts/default/");
 	$text = strip_tags($text, '<br>');
 	$text = util_split("/(homework:|homework)/i", $text, 1, 1);
 	echo_json(finalize($text, '<br>'), "http://fireteamhistory.blogspot.com/");
