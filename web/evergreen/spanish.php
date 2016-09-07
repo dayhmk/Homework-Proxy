14729f0f143571c55244994a3785636fd560a5b5
DayHMK <dayhomework@gmail.com>
2016-09-07 15:58:58 -0400

    Fixed Evergreen Spanish script

--- web/evergreen/spanish.php
+++ web/evergreen/spanish.php
@@ -1,8 +1,11 @@
 <?php
 	header('Access-Control-Allow-Origin: https://dayhmk.github.io');
 	require '../utils.php';
+
 	$text = file_get_contents("http://www2.newton.k12.ma.us/~courtney_fournier/?OpenItemURL=S08BD3217");
-	$text = util_split('/(enero|febrero|marzo|abril|mayo |junio|julio|agosto|septiembre|octubre|noviembre|diciembre)/i', $text, -1, 0);
-	$text = util_split('/<div[^>]*>/i', $text, -4, -1);
+	$text = util_split('/(La Plaza, Mayor, Salamanca)/i', $text, 0, 0);
+	$text = util_split('/(enero|febrero|marzo|abril|mayo|junio|julio|agosto|septiembre|octubre|noviembre|diciembre)(<\/b>|<\/u>)/i', $text, -1, 0);
+	$text = util_split('/<img[^>]*>/i', $text, -2, -1);
+
 	echo_json(finalize($text, '<br>'), "http://www2.newton.k12.ma.us/~courtney_fournier/?OpenItemURL=S08BD3AB2");
 ?>
